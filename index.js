25bffdca6d30ea2c12e3e724f702df8d5ffbf321
Md. Al Asad Nur Riyad <asadnurriyad@gmail.com>
2022-07-31 12:54:17 +0600

    Sreate basic server
    Signed-off-by: Md. Al Asad Nur Riyad <asadnurriyad@gmail.com>

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("riyad");
+const http = require("http");
+
+// Module Scaffolding
+const app = {};
+
+// Configaretion
+app.config = {
+  port: 3000,
+};
+
+// Create Server
+app.createServer = () => {
+  const server = http.createServer(app.handleReqRes);
+  server.listen(app.config.port, () => {
+    console.log(`Listening to port ${app.config.port}`);
+  });
+};
+
+// Handle Req and Res
+app.handleReqRes = (req, res) => {
+  res.end("Hello world");
+};
+
+// Run the server
+app.createServer();
