1e751de6fb3ebc9dfb064f3d8c4d65d08123f7df
Md. Al Asad Nur Riyad <asadnurriyad@gmail.com>
2022-08-20 15:50:42 +0600

    add trwilo to send notification
    Signed-off-by: Md. Al Asad Nur Riyad <asadnurriyad@gmail.com>

---
+++ helper/notification.js
@@ -0,0 +1,50 @@
+const https = require("https");
+const querystring = require("querystring");
+const { twilio } = require("./enviroment");
+
+const notification = {};
+
+notification.sendMsg = (phone, msg, callback) => {
+  const Phone =
+    typeof phone === "string" && phone.length === 11 ? phone : false;
+  const Msg = typeof msg === "string" && msg.trim().length > 0 ? msg : false;
+
+  if (Phone && Msg) {
+    const payLoad = {
+      From: twilio.fromPhone,
+      To: `+88${Phone}`,
+      Body: Msg,
+    };
+
+    const stringifyPayload = querystring.stringify(payLoad);
+
+    const reqDetailsObj = {
+      hostname: "api.twilio.com",
+      method: "POST",
+      path: `/2010-04-01/Accounts/${twilio.accountSid}/Messages.json`,
+      auth: `${twilio.accountSid}:${twilio.authToken}`,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    };
+
+    const req = https.request(reqDetailsObj, (res) => {
+      const { statusCode } = res;
+      if (statusCode === 200 || statusCode === 201) {
+        callback(false);
+      } else
+        callback({
+          Error: `message not properly send status code ${statusCode}`,
+        });
+    });
+
+    req.write(stringifyPayload);
+    req.end();
+
+    req.on("error", (err) => {
+      callback({ Error: err });
+    });
+  } else callback({ Error: "Send valid phone and msg" }, "");
+};
+
+module.exports = notification;
